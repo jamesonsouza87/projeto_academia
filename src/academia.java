086e093ee385a6edb3e4638fec4018d0cfba8a22
jameson souza <jamesonbatista25@gmail.com>
2022-04-04 22:26:47 -0300

    remoção de classes  do antigo projeto
    remoção de classes do antigo projeto

--- src/academia.java
+++ src/academia.java
@@ -3,41 +3,16 @@ public class academia {
     public static void main(String[] args) {
         System.out.println("Seja Bem Vindo ! ");
     }
-
-    public class VagasDesponiveis {
+    public class Cadastro {
         public static void main(String[] args) {
-            int TotalDeVagas = 100;
-            int VagasP1 = 25;
-            int VagasP2 = 25;
-            int VagasP3 = 25;
-            int VagasP4 = 25;
-            System.out.println("escolha uma de nossas vagas disponiveis no momento. ");
+            String Nome;
+            int CPF;
+            int Fone;
+            int Placa;
+            String Diaria;
+            String Semanal;
+            String Mensal;
+            String Anual;
         }
 
-        public class VagasIndisponivel {
-            public static void main(String[] args) {
-                boolean aguardar = true;
-                boolean EstacionamentoLotado = true;
-                boolean VagasDesponiveis = false;
-                System.out.println("lamentamos mas não temos vagas disponiveis no momento!");
-                if (aguardar) {
-                    System.out.println("aguarde um instante vamos verificar em quanto tempo teremos vagas disponiveis");
-                    return;
-                }
-
-            }     public class Cadastro {
-                    public static void main(String[] args) {
-                        String Nome;
-                        int CPF;
-                        int Fone;
-                        int Placa;
-                        String Diaria;
-                        String Semanal;
-                        String Mensal;
-                        String Anual;
-                    }
-                }
-            }
-        }
-    }
-}
+    }}
